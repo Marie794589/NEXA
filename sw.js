b92b50940d4758781f637b04b34da1f9da475477
Marie794589 <mleyandeabellard@gmail.com>
2026-04-24 15:05:23 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'nexa-v1';
+const ASSETS = [
+  '/',
+  '/manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Réponse aux requêtes pour permettre le mode hors-ligne basique
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
